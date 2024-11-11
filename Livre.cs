ed3bddd015f238060dae758b08591b612fff76a5
Joseph <300137754@monboreal.ca>
2024-11-11 17:05:12 -0500

    Livre.cs modifié

--- Livre.cs
+++ Livre.cs
@@ -8,5 +8,26 @@ namespace AppConsoleLivre2
 {
     internal class Livre
     {
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int NombrePages { get; set; }
+
+
+        public Livre(string titre, string auteur, int nbrPages)
+        {
+
+            Titre = titre;
+            Auteur = auteur;
+            NombrePages = nbrPages;
+
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"Le livre s'appelle: {Titre} écrit par {Auteur} avec un total de {NombrePages} pages");
+        }
+
+
+
     }
 }
