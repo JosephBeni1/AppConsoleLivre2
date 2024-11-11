194c89baecd2111cd9160e46ce9476f5e5cd9ef0
Joseph <300137754@monboreal.ca>
2024-11-11 17:07:41 -0500

    Revue.cs enregistré

---
+++ Revue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivre2
+{
+    internal class Revue : Livre
+    {
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+
+        public Revue(string titre, string auteur, int nbrPages, int numero, int annee) : base(titre, auteur, nbrPages)
+        {
+
+            Numero = numero;
+            Annee = annee;
+        }
+
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Le titre du livre est: {Titre}, rédigé par {Auteur} en {Annee}, contenant {NombrePages}, il s'agit du {Numero} de l'auteur");
+        }
+    }
+}
