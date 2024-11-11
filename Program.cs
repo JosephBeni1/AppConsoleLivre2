6aae4092f7ec9595b9992bf65f03721d6a6bd243
Joseph <300137754@monboreal.ca>
2024-11-11 17:21:09 -0500

    Progam.cs enregistré

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using AppConsoleLivre2;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Revue rev1 = new Revue("Disparition programmée", "Roland Smith", 316, 2, 2013);
+        Revue rev2 = new Revue("Elia, la passeuse d'âmes", "Marie Vareille", 307, 3, 2016);
+        Revue rev3 = new Revue("Kinshasa Dream", "Anna Kuschnarowa", 324, 4, 2016);
+
+        rev1.AfficherDetails();
+        rev2.AfficherDetails();
+        rev3.AfficherDetails();
+
+
     }
 }
